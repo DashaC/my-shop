558212236223241979373d932d9ef29d70cd86b5
DashаС <churikovadarya@gmail.com>
2019-05-14 15:42:52 +0300

    create new separate components Header, Main and Footer

---
+++ src/App/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './header.css';
+import './cart.css';
+import './menu.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="logo">
+              <img src="img/logo.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-6">
+            <div className="menu">
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Product</a></li>
+                <li><a href="">Payment</a></li>
+                <li><a href="">Shipping</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-3">
+            <div className="cart text-center">
+              <div className="products-count">0</div>
+              <div className="products-price">0$</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
