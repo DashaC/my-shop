89d36b26cd6591f7bae4e96d97a62364d8abd9c4
DashаС <churikovadarya@gmail.com>
2019-05-15 11:43:42 +0300

    add ProductList and ProductListItem components

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
+import ProductsList from './Products/ProductsList';
+
 const Main = () => {
   return (
     <main className="main">
       <div className="container">
         <div className="row">
           <div className="col-lg-3">
-            
           </div>
           <div className="col-lg-9">
-            
+            <ProductsList />    
           </div>
         </div>
       </div>
