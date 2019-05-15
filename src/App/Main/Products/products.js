545766590bd5c7a5d62cff7a0c6218144c791147
DashаС <churikovadarya@gmail.com>
2019-05-15 13:34:44 +0300

    add unique keys to products

--- src/App/Main/Products/products.js
+++ src/App/Main/Products/products.js
@@ -4,28 +4,32 @@ const products = [
 		description: 'This is iPhone 7',
 		type: 'phone',
 		capacity: 64,
-		price: 500
+		price: 500,
+		id: 1
 	},
 	{
 		name: 'iPhone 8',
 		description: 'This is iPhone 8',
 		type: 'phone',
 		capacity: 128,
-		price: 700
+		price: 700,
+		id: 2
 	},
 	{
 		name: 'iPhone X',
 		description: 'This is iPhone X',
 		type: 'phone',
 		capacity: 256,
-		price: 900
+		price: 900,
+		id: 3
 	},
 	{
 		name: 'iPhone XS',
 		description: 'This is iPhone XS',
 		type: 'phone',
 		capacity: 256,
-		price: 1000
+		price: 1000,
+		id: 4
 	}
 ];
 
